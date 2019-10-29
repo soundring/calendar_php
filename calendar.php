fba0ca1ec2fe32f60c1b03e212a9d0f09abb4675
soundring <soundringing@yahoo.co.jp>
2019-10-29 12:32:34 +0900

    スタイルのみ

--- calendar.php
+++ calendar.php
@@ -3,12 +3,39 @@
 <head>
     <meta charset="utf-8">
     <title>PHPカレンダー</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <style>
+        .container {
+            font-family: 'Noto Sans JP', sans-serif;
+            margin-top: 80px;
+        }
+        h3 {
+            margin-bottom: 30px;
+        }
+        th {
+            height: 30px;
+            text-align: center;
+        }
+        td {
+            height: 100px;
+        }
+        .today {
+            background-color: orange;
+        }
+        th:nth-of-type(1), td:nth-of-type(1) {
+            color: red;
+        }
+        th:nth-of-type(7), td:nth-of-type(7) {
+            color: blue;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <h3><a>&lt;</a>&nbsp;&nbsp;2019年 1月&nbsp;&nbsp;<a>&gt;</a></h3>
 
-        <table class="table.table-bordered">
+        <table class="table table-bordered">
             <tr>
                 <th>日</th>
                 <th>月</th>
@@ -31,7 +58,7 @@
                 <td>8</td>
                 <td>9</td>
                 <td>10</td>
-                <td>11</td>
+                <td class="today">11</td>
                 <td>12</td>
                 <td>13</td>
                 <td>14</td>
